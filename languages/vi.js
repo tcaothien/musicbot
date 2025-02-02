735cd049157dc318fbdb747bae5501b73cc30333
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-02-02 13:51:49 +0700

    Update vi.js

--- languages/vi.js
+++ languages/vi.js
@@ -1,5 +1,5 @@
 module.exports = {
-    footer: "Phát triển bởi New Life | Prime Music v1.2",
+    footer: "Phát triển bởi New Life | New Life Music v1.2",
     ping: {
       description: "Kiểm tra độ trễ của bot",
       response: "Đang đo độ trễ...",
@@ -8,7 +8,7 @@ module.exports = {
         responseTime: "- Thời gian phản hồi của bot: **{latency}ms**",
         websocketPing: "- Độ trễ WebSocket: **{ping}ms**",
         uptime: "- Thời gian hoạt động: **{uptime}**",
-        footer: "Phát triển bởi SSRR | Prime Music v1.2"
+        footer: "Phát triển bởi Gwwennn | New Life Music v1.2"
       }
     },
     addsong: {
