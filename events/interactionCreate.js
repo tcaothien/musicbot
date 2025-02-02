bf1204237953ea4cabb804d9494626c58c0bebdb
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-02-02 13:47:08 +0700

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,7 +6,7 @@ const path = require("path");
 module.exports = async (client, interaction) => {
   try {
     if (!interaction?.guild) {
-      return interaction?.reply({ content: "This command can only be used in a server.", ephemeral: true });
+      return interaction?.reply({ content: "Lệnh này chỉ có thể được sử dụng trong máy chủ.", ephemeral: true });
     }
 
     const languageFile = path.join(__dirname, `../languages/${config.language}.js`);
@@ -23,10 +23,10 @@ module.exports = async (client, interaction) => {
                 if (interaction?.member?.permissions?.has(props?.permissions || "0x0000000000000800")) {
                   return props.run(client, interaction, lang);
                 } else {
-                  return interaction?.reply({ content: lang.errors.noPermission, ephemeral: true });
+                  return interaction?.reply({ content: "Bạn không có quyền sử dụng lệnh này!", ephemeral: true });
                 }
               } catch (e) {
-                return interaction?.reply({ content: lang.errors.generalError.replace("{error}", e.message), ephemeral: true });
+                return interaction?.reply({ content: `Đã xảy ra lỗi: ${e.message}`, ephemeral: true });
               }
             }
           });
