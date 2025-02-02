2f9b42c167ca2f34d801d0f5d8a6271fc6038e02
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-02-02 13:36:01 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ const musicIcons = require('../UI/icons/musicicons.js');
 
 module.exports = {
   name: "ping",
-  description: "Check the bot latency",
+  description: "Kiểm tra độ trễ của bot",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction, lang) => {
