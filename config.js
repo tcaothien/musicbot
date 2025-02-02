73879a27d0682bae1f5227b7edbdc00c986a0121
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-02-02 11:04:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/NewLifes",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
