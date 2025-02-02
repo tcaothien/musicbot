28a567157c39f4db62c69d87cac398b8a48c09d7
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-02-02 13:32:03 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ const musicIcons = require('../UI/icons/musicicons.js');
 
 module.exports = {
   name: "help",
-  description: "Get information about the bot",
+  description: "thông tin về bot",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction, lang) => {
