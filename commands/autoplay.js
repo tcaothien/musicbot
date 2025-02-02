a890bbfb14dd35458f283bd1bc1937851ff47e85
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-02-02 13:25:56 +0700

    Update autoplay.js

--- commands/autoplay.js
+++ commands/autoplay.js
@@ -17,27 +17,27 @@ async function toggleAutoplay(client, interaction, lang) {
         const embed = new EmbedBuilder()
             .setColor(enable ? '#00ff00' : '#ff0000')
             .setAuthor({ 
-                name: lang.autoplay.embed.autoplayUpdated, 
+                name: "Cập nhật chế độ tự động phát", 
                 iconURL: musicIcons.correctIcon,
                 url: config.SupportServer 
             })
-            .setFooter({ text: lang.footer, iconURL: musicIcons.heartIcon })
+            .setFooter({ text: "Hỗ trợ", iconURL: musicIcons.heartIcon })
             .setTimestamp()
-            .setDescription(lang.autoplay.embed.autoplayStatus.replace("{status}", enable ? lang.autoplay.embed.enabled : lang.autoplay.embed.disabled));
+            .setDescription(`Chế độ tự động phát đã được ${enable ? "bật" : "tắt"}.`);
 
         await interaction.reply({ embeds: [embed] });
     } catch (error) {
-        console.error('Error toggling autoplay:', error);
+        console.error('Lỗi khi chuyển đổi chế độ tự động phát:', error);
         const errorEmbed = new EmbedBuilder()
             .setColor('#ff0000')
             .setAuthor({ 
-                name: lang.autoplay.embed.error, 
+                name: "Lỗi", 
                 iconURL: musicIcons.alertIcon,
                 url: config.SupportServer 
             })
-            .setFooter({ text: lang.footer, iconURL: musicIcons.heartIcon })
+            .setFooter({ text: "Hỗ trợ", iconURL: musicIcons.heartIcon })
             .setTimestamp()
-            .setDescription(lang.autoplay.embed.errorDescription);
+            .setDescription("Đã xảy ra lỗi khi cập nhật chế độ tự động phát. Vui lòng thử lại sau.");
 
         await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
     }
@@ -45,12 +45,12 @@ async function toggleAutoplay(client, interaction, lang) {
 
 module.exports = {
     name: 'autoplay',
-    description: 'Toggle autoplay for the server',
+    description: 'Bật hoặc tắt chế độ tự động phát trong máy chủ',
     permissions: '0x0000000000000800',
     options: [
         {
             name: 'enable',
-            description: 'toggle autoplay on / off',
+            description: 'Bật / Tắt chế độ tự động phát',
             type: ApplicationCommandOptionType.Boolean,
             required: true
         }
