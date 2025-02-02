43c82cc78052037889447cb43fd27909cea52f09
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-02-02 13:12:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "Koi Node V4",
-      password: "prplmoe.me",
-      host: "lavav4.prplmoe.me",
-      port: 1118,
+      name: "INZEWORLD.COM (DE)",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port:  3128,
       secure: false
     },
     {
