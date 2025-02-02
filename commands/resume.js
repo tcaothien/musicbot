8d87d0c7212beb3b940eb9f72c85cf7a1bc2cd90
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-02-02 13:41:10 +0700

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -53,7 +53,7 @@ async function resume(client, interaction, lang) {
 
 module.exports = {
     name: "resume",
-    description: "Resume the current song",
+    description: "Tiếp tục bài hát hiện tại",
     permissions: "0x0000000000000800",
     options: [],
     run: resume
